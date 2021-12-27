8070ca930384ddecb21a12a717c5084cb12c6d05
sweta1312 <swetaverma131296@gmail.com>
2021-12-27 19:12:03 +0530

    oops/27Dec

---
+++ src/exceptions/ExceptionCases.java
@@ -0,0 +1,46 @@
+package exceptions;
+
+public class ExceptionCases {
+    public static void main(String[] args) {
+      /* //case1
+        try{}
+        catch(Exception e){}
+        finally {
+
+        }
+        //case2
+        try{}
+        finally {
+
+        }
+        //case3
+        try{}
+        catch (ArithmeticException ae){}
+        catch (Exception e){}*/
+
+        //case4 error finally without try
+        //try{}
+     //  finally{}
+        //case5
+       // catch(Exception e){} //error
+        //case6 //error
+      /*  try {
+        }*/
+
+        //case7 error
+     /*   try{ }
+        finally {
+        catch (Exception e){}*/
+
+        //case8 error
+   /*     try{}
+        int a=10;
+        catch(Exception e){}
+        int b=20;
+        finally{}
+        */
+
+        }
+
+    }
+
